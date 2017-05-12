a08365d1552b3374b88282528ddd999a1bf9540e
Akash <akashbaga011@gmail.com>
2017-05-12 13:15:04 +0300

    user interface

---
+++ main.py
@@ -0,0 +1,32 @@
+#import calender_events.py
+
+my_bool = True
+while True:
+	print ("Welcome to Caleder:")
+	print ("Calender Actions:")
+	print ("1. Create Calender")
+	print ("2. Add Task to calender")
+	print ("3. View all tasks in caleneder")
+	print ("4. View last Task")
+	print ("5. QUIT!")
+	print ("Please enter a number corresponing to the action: ")
+	user_input = input("Please Enter a Number that corresponds to the action: " )
+
+	if user_input == '5':
+		break 
+	elif user_input == '1':
+		print ("it works")
+	elif user_input == '2':
+		username = input("Please enter username: ")
+		date = input("Please enter date:" )
+		event = input("Please enter event: ")
+		creat_event(username,date,event)
+	elif user_input == '3':
+		#
+		#
+	elif user_input == '4':
+		#
+		#
+
+
+ 
